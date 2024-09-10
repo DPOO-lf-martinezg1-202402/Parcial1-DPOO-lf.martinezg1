d659cfcf6d046181e0d549b75060390cc1fcecb1
Laura-Martinez-Galindo <lf.martinezg1@uniandes.edu.co>
2024-09-10 09:20:03 -0500

    ya reina

--- src/logica/Reina.java
+++ src/logica/Reina.java
@@ -1,5 +1,6 @@
 package logica;
 
+
 public class Reina extends Pieza{
 
 	public Reina(int fila, int columna) {
@@ -9,15 +10,26 @@ public class Reina extends Pieza{
 	@Override
 	public int contarCasillas() {
 		final int tamaño = 8;
+		int casillasHorizontal = 0;
+		int casillasVertical = 0;
+		int casillasDiagonalPrincipal = 0;
+		int casillasDiagonalSecundaria = 0;
 		
-		int casillasHorizontal = (tamaño - this.columna);
-		int casillasVertical = (tamaño - this.columna);
-		//por cada horizontal y vertical hay una diagonal
-		int casillasDiagonalPrincipal = ();
-		int casillasDiagonalSecundaria = ;
+		if (this.fila <= tamaño && this.fila >= 1 && this.columna <= tamaño && this.columna >= 1) {
+			//adelante
+			casillasHorizontal += (tamaño - this.fila);
+			casillasVertical += (tamaño - this.columna);
+			//atras
+			casillasHorizontal += this.fila - 1;
+			casillasVertical += this.columna - 1;
+			//diagonal principal, si horizontal y vertical adelante, o ambas hacia atras
+			casillasDiagonalPrincipal += (casillasHorizontal + casillasVertical)/2;
+			//diagonal secundaria, minimo entre fila y columna por delante y por detras
+			casillasDiagonalSecundaria += Math.min(tamaño-this.fila, tamaño-this.columna);
+			casillasDiagonalSecundaria += Math.min(this.fila-1, this.columna-1);
+		}
 		
-		return casillasHorizontal + casillasVertical;
-		return 0;
+		return casillasHorizontal + casillasVertical + casillasDiagonalPrincipal + casillasDiagonalSecundaria;
 	}
 
 }
